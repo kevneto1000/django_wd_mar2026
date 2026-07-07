7a709194d22262ae2d20c44417a1af90c12686b0
neto <netodboss@gmail.com>
2026-07-07 14:39:41 +0100

    Configure default storage for media uploads

--- core/settings.py
+++ core/settings.py
@@ -181,6 +181,9 @@ USE_TZ = True
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STORAGES = {
+    'default': {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     'staticfiles': {
         "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
