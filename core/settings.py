51c1ee6d43edb48fa8419df3b1d520d3692bdfa6
neto <netodboss@gmail.com>
2026-07-09 14:54:27 +0100

    testing cloudinary

--- core/settings.py
+++ core/settings.py
@@ -17,16 +17,6 @@ import cloudinary
 
 load_dotenv()
 
-cloudinary.config(
-    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
-    api_key=os.getenv("CLOUDINARY_API_KEY"),
-    api_secret=os.getenv("CLOUDINARY_API_SECRET"),
-    secure=True,
-)
-
-print("CLOUDINARY_URL:", os.getenv("CLOUDINARY_URL"))
-print("CLOUDINARY_API_KEY:", os.getenv("CLOUDINARY_API_KEY"))
-print("CLOUDINARY_CLOUD_NAME:", os.getenv("CLOUDINARY_CLOUD_NAME"))
 
 from pathlib import Path
 from datetime import timedelta
